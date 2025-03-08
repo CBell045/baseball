4f055b9bad256cb8d1b28251c32b2cf5d8913cfd
Chad <chadbell045@gmail.com>
2025-03-08 17:00:40 -0600

    finished basic arima model

--- Home.py
+++ Home.py
@@ -1,40 +1,71 @@
 import streamlit as st
-import polars as pl
-import altair as alt
+import pybaseball
+from statsmodels.tsa.arima.model import ARIMA
+import pandas as pd
+import plotly.express as px
 
-# Sample batting average data
-df = pl.DataFrame(
-    {
-        "Player": [
-            "Mike Trout",
-            "Mookie Betts",
-            "Aaron Judge",
-            "Shohei Ohtani",
-            "Freddie Freeman",
-        ],
-        "Batting Average": [0.312, 0.292, 0.287, 0.280, 0.305],
-    }
-)
+pd.options.mode.copy_on_write = True
+
+
+@st.cache_data()
+def load_data():
+    # Load data from pybaseball
+    return pybaseball.batting_stats(2000, 2024)
+
 
 st.header("Batting Average Prediction ⚾")
 
+data = load_data()
+
 # Dropdown to select player(s)
-players = st.multiselect("Select Player", df["Player"].to_list())
+player: str = st.selectbox("Select Player", data["Name"].unique())
 
-if players:
+if player:
     # Filter data based on selected player(s)
-    df = df.filter(df["Player"].is_in(players))
+    player_df = data[data["Name"] == player]
 
-# Create bar chart
-chart = (
-    alt.Chart(df)
-    .mark_bar()
-    .encode(
-        x=alt.X("Player", sort="-y"),
-        y=alt.Y("Batting Average", title="Batting Average"),
+    # Player df sort
+    player_df = player_df.sort_values(by="Season")
+
+    # Dropdown to select year
+    year = st.selectbox(
+        "Select Year", sorted(player_df["Season"].unique().tolist(), reverse=True)
     )
-    .properties(title="MLB Players - Batting Averages")
-)
 
-# Display chart in Streamlit
-st.altair_chart(chart, use_container_width=True)
+    if year:
+        # Filter data based on selected year
+        df = player_df[player_df["Season"] < year]
+
+        df["Season"] = pd.to_datetime(df["Season"], format="%Y")
+        df = df.set_index("Season").sort_index().asfreq("YS")
+
+        # Predict
+        model = ARIMA(df["AVG"], order=(1, 1, 1))
+        fitted_model = model.fit()
+
+        forecast = fitted_model.forecast(steps=1)
+
+        # Display prediction
+        st.write(f"Predicted Batting Average for {year}: {forecast.iloc[0]:.3f}")
+
+        st.write(
+            f"Actual Batting Average for {year}: {player_df[player_df['Season'] == year]['AVG'].values[0]:.3f}"
+        )
+
+        # Create chart
+        fig = px.line(
+            player_df,
+            x="Season",
+            y="AVG",
+            title=f"Batting Average Prediction for {player}",
+        )
+
+        fig.add_scatter(
+            x=[year],
+            y=[forecast.iloc[0]],
+            mode="markers",
+            marker=dict(color="red", size=10),
+            name="Prediction",
+        )
+
+        st.plotly_chart(fig)
