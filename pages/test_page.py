f969c731528007c867712b1b56603fe95a5cf3cc
Chad <chadbell045@gmail.com>
2025-03-27 19:38:03 -0500

    testing statsforecast

---
+++ pages/test_page.py
@@ -0,0 +1,10 @@
+import statsforecast.models
+models = [
+    statsforecast.models.AutoARIMA(),
+    # statsforecast.models.AutoETS(),
+    # statsforecast.models.AutoRegressive(10),
+    # statsforecast.models.HoltWinters(),
+    # statsforecast.models.HistoricAverage(),
+]
+
+print("yay!")
